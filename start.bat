673a044a480e9313f09c0cd01c0026b706501da6
taha İPEK <ti.tahaipek@gmail.com>
2016-06-13 00:12:31 +0300

    Overwrite all file, start Microsoft Edge.

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
-xcopy "VisualStudioActivityMonitorLog" "C:\inetpub\wwwroot\VisualStudioActivityMonitorLog\" /s/h/e/k/f/c
-xcopy "%userprofile%\AppData\Roaming\Microsoft\VisualStudio\14.0\ActivityLog.xml" "C:\inetpub\wwwroot\VisualStudioActivityMonitorLog\ActivityLog.xml"*
-start iexplore.exe "http://localhost/VisualStudioActivityMonitorLog/ActivityLog.xml"
+xcopy "VisualStudioActivityMonitorLog" "C:\inetpub\wwwroot\VisualStudioActivityMonitorLog\" /s/h/e/k/f/c/y
+xcopy "%userprofile%\AppData\Roaming\Microsoft\VisualStudio\14.0\ActivityLog.xml" "C:\inetpub\wwwroot\VisualStudioActivityMonitorLog\ActivityLog.xml"*/y
+start microsoft-edge:"http://localhost/VisualStudioActivityMonitorLog/ActivityLog.xml"
